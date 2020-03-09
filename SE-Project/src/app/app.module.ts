e4b603115f1dab7b37ff36250e3a77ffb9550107
Samantha Kreisel <samikreisel@gmail.com>
2020-03-08 19:51:03 -0500

    add pages

--- SE-Project/src/app/app.module.ts
+++ SE-Project/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import { TitlePageComponent } from './title-page/title-page.component';
+import { PlaylistComponent } from './playlist/playlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    TitlePageComponent,
+    PlaylistComponent
   ],
   imports: [
     BrowserModule,
